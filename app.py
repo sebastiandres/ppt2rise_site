f3987440a0b7ff3f0f4fe664bc2ea8c38ec2bfe8
sebastiandres <sebastiandres@gmail.com>
2021-05-17 23:23:24 -0400

    A working version with file upload

--- app.py
+++ app.py
@@ -3,24 +3,38 @@ from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
-
+from glob import glob
 # See
 # https://blog.miguelgrinberg.com/post/handling-file-uploads-with-flask
 # https://j2logo.com/tutorial-flask-leccion-4-login/
 
-app.config['UPLOAD_PATH'] = 'input_files'
+app.config['UPLOAD_PATH'] = 'pptx_files'
 app.config['MAX_CONTENT_PATH'] = 1024*1024
 
-for k,v in app.config.items():
-   print(k, v)
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+@app.route('/pptx_files')
+def pptx_files():
+   pptx_files = {"name":"pptx files", "list":[]}
+   pptx_files["list"] = glob("pptx_files/*.*")
+   print(pptx_files)
+   return render_template('pptx_files.html', files=pptx_files)
+
+@app.route('/ipynb_files')
+def ipynb_files():
+   ipynb_files = {"name":"ipynb files", "list":[]}
+   ipynb_files["list"] = glob("ipynb_files/*.*")
+   print(ipynb_files)
+   return render_template('ipynb_files.html', files=ipynb_files)
 
 @app.route('/upload')
-@app.route('/')
-def upload_file():
+def upload():
    return render_template('upload.html')
-	
+
 @app.route('/uploaded', methods = ['GET', 'POST'])
-def uploader_file():
+def uploaded():
    if request.method == 'POST':
       f = request.files['file']
       filename = os.path.join(app.config['UPLOAD_PATH'] , secure_filename(f.filename) )
