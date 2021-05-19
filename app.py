15c10020abc696d07f4bdbf47d8accb57c52558f
sebastiandres <sebastiandres@gmail.com>
2021-05-18 22:20:57 -0400

    some version that allows conversion

--- app.py
+++ app.py
@@ -1,31 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from werkzeug.utils import secure_filename
 import os
+from pptx_to_RISE import ppt2rise
+import shutil
+from glob import glob
+from datetime import datetime
 
 app = Flask(__name__)
-from glob import glob
 # See
 # https://blog.miguelgrinberg.com/post/handling-file-uploads-with-flask
 # https://j2logo.com/tutorial-flask-leccion-4-login/
 
-app.config['UPLOAD_PATH'] = 'pptx_files'
-app.config['MAX_CONTENT_PATH'] = 1024*1024
+app.config['PPTX_PATH'] = 'files/pptx_files/'
+app.config['IPYNB_PATH'] = 'files/ipynb_files/'
+app.config['ZIP_PATH'] = 'files/zip_files/'
+app.config['ALLOWED_EXTENSIONS'] = ['ipynb',]  # Not used
+app.config['MAX_CONTENT_PATH'] = 1024*1024 # Not used
+
+def make_archive(source, destination):
+   base = os.path.basename(destination)
+   name = base.split('.')[0]
+   format = base.split('.')[1]
+   archive_from = os.path.dirname(source)
+   archive_to = os.path.basename(source.strip(os.sep))
+   print(source, destination, archive_from, archive_to)
+   shutil.make_archive(name, format, archive_from, archive_to)
+   shutil.move('%s.%s'%(name,format), destination)
 
 @app.route('/')
 def index():
    return render_template('index.html')
-
+   
 @app.route('/pptx_files')
 def pptx_files():
    pptx_files = {"name":"pptx files", "list":[]}
-   pptx_files["list"] = glob("pptx_files/*.*")
+   pptx_files["list"] = glob(app.config['PPTX_PATH'] + "/*.pptx")
    print(pptx_files)
    return render_template('pptx_files.html', files=pptx_files)
 
 @app.route('/ipynb_files')
 def ipynb_files():
    ipynb_files = {"name":"ipynb files", "list":[]}
-   ipynb_files["list"] = glob("ipynb_files/*.*")
+   ipynb_files["list"] = glob(app.config['IPYNB_PATH'] + "/*.ipynb")
    print(ipynb_files)
    return render_template('ipynb_files.html', files=ipynb_files)
 
@@ -36,10 +52,43 @@ def upload():
 @app.route('/uploaded', methods = ['GET', 'POST'])
 def uploaded():
    if request.method == 'POST':
+      # Get the file properties
       f = request.files['file']
-      filename = os.path.join(app.config['UPLOAD_PATH'] , secure_filename(f.filename) )
-      f.save(filename)
-      return 'file uploaded successfully'
+      filename = secure_filename(f.filename)
+      basename = filename.replace(".pptx", "")
+      # Timestamp to dynamic path
+      timestamp_str = datetime.now().strftime("%Y-%b-%d-%H-%M-%S")
+      # Main path
+      global_path = os.getcwd()
+      # pptx files and paths
+      pptx_filename = filename
+      pptx_basepath = os.path.join(global_path, app.config['PPTX_PATH'])
+      pptx_filepath = os.path.join(pptx_basepath, pptx_filename)
+      print("pptx_filepath", pptx_filepath)
+      # ipynb files and paths
+      ipynb_filename = basename + ".ipynb"
+      ipynb_basepath = os.path.join(global_path, app.config['IPYNB_PATH'] , basename + timestamp_str)
+      ipynb_filepath = os.path.join(ipynb_basepath, ipynb_filename)
+      print("ipynb_filepath", ipynb_filepath)
+      # zip files and paths
+      zip_filename = basename + timestamp_str + ".zip"
+      zip_basepath = os.path.join(global_path, app.config['ZIP_PATH'])
+      zip_filepath = os.path.join(zip_basepath , zip_filename)
+      print("zip_filepath", zip_filepath)
+      try:
+         os.mkdir(ipynb_path)
+      except:
+         print("Dir exists?")
+      # Store in folder
+      f.save(pptx_filepath)
+      # Convert
+      ppt2rise.ppt2rise(pptx_filepath, ipynb_filepath)  
+      # Zip
+      make_archive(ipynb_basepath, zip_filepath)
+      # Offer to download it
+      #print(send_from_directory(directory=zip_basepath, filename=zip_filename))  
+      return send_from_directory(zip_basepath, zip_filename, as_attachment=True)
+
 		
 if __name__ == '__main__':
    app.run(debug = True)
